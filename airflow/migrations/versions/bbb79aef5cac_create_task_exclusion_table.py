0e39077d6adec869d9869d51ca3424a26932878e
robin_miller_ow <robin.miller@affiliate.oliverwyman.com>
2016-11-18 13:59:02 +0000

    Add migration script for task_exclusion table.

---
+++ airflow/migrations/versions/bbb79aef5cac_create_task_exclusion_table.py
@@ -0,0 +1,47 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""create task_exclusion table
+
+Revision ID: bbb79aef5cac
+Revises: f2ca10b85618
+Create Date: 2016-11-18 13:38:34.653202
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'bbb79aef5cac'
+down_revision = 'f2ca10b85618'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    ID_LEN = 250
+    op.create_table(
+        'task_exclusion',
+        sa.Column('dag_id', sa.String(ID_LEN), nullable=False),
+        sa.Column('task_id', sa.String(ID_LEN), nullable=False),
+        sa.Column('exclusion_type', sa.String(32), nullable=False),
+        sa.Column('exclusion_start_date', sa.DateTime, nullable=False),
+        sa.Column('exclusion_end_date', sa.DateTime, nullable=False),
+        sa.Column('created_by', sa.String(256), nullable=False),
+        sa.Column('created_on', sa.DateTime, nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table('task_exclusion')
