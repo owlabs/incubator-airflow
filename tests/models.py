5c86eae0742a0207d69164e599e646312a3096ae
luke.maycock <luke.maycock@bluesmithis.com>
2016-12-09 10:31:45 +0000

    Copied date from another unit test.

--- tests/models.py
+++ tests/models.py
@@ -629,7 +629,8 @@ class TaskInstanceTest(unittest.TestCase):
 
 class TaskExclusionTest(unittest.TestCase):
     session = settings.Session()
-    exec_date = datetime.datetime(2016, 12, 8, 17, 37, 3, 111111)
+    exec_date = datetime.datetime(2016, 1, 1)
+
 
     def SetUp(self):
         # Obtain all exclusions
